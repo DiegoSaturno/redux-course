68cfe284a00834f6e840bdb5b329b3b81e09a3be
Diego Oliveira <diegooliveira@ciandt.com>
2021-12-07 17:30:31 -0300

    starting repo

---
+++ comments/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import faker from 'faker'
+
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Samus
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 5pm</span>
+          </div>
+          <div className="text">Heh. Nice.</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
